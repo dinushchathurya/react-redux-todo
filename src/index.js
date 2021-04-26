7f84f62775002ba7b42ba50f355390b8e8f0ad52
dinushchathurya <dinushchathurya21@gmail.com>
2021-04-26 21:35:05 +0530

    feat: perring createStore to index.js

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import tasks from './reducers';
 import reportWebVitals from './reportWebVitals';
+
+const store = createStore(tasks);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
