ce9e20b28923bd0418316e50c7d6ca816a0af8f4
dinushchathurya <dinushchathurya21@gmail.com>
2021-04-26 21:35:25 +0530

    feat; create dummy reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+const initialState = [
+    {
+        id:1,
+        title: "Title One",
+        description: "Description One"
+    }
+]
+
+const tasks = (state= { tasks: initialState}, action) => {
+    return state;
+}
+
+export default tasks;
